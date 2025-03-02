e6fa33c4fd7a4e5b55d165abbac061df926b8ac5
Iyare Usen <iyare.usen@gmail.com>
2025-03-02 02:38:14 +0100

    accounts for empty or invalid user inputs

--- script.js
+++ script.js
@@ -18,59 +18,69 @@ const weightResults = document.querySelector("#weightResults");
 const volumeResults = document.querySelector("#volumeResults");
 let inputedValue = 0;
 
-userInput.addEventListener("change", (e) => {
-   inputedValue = Number(e.target.value);
-   const metrics = convertMetrics(inputedValue);
-   renderResults(metrics);
-})
+// userInput.addEventListener("change", (e) => {
+//   inputedValue = Number(e.target.value);
+//   const metrics = convertMetrics(inputedValue);
+//   renderResults(metrics);
+// });
 
-userInput.addEventListener("keypress", (e)=> {
-    const keyName = e.key;
-    if(keyName === "Enter"){
-        inputedValue = userInput.value;
-        const metrics = convertMetrics(inputedValue);
-        renderResults(metrics);
-    }
-})
+userInput.addEventListener("keypress", (e) => {
+  const keyName = e.key;
+  if (keyName === "Enter") {
+    inputedValue = userInput.value;
+    const metrics = convertMetrics(inputedValue);
+    renderResults(metrics);
+  }
+});
 
 convertBtn.addEventListener("click", function () {
   inputedValue = userInput.value;
+
   const metrics = convertMetrics(inputedValue);
   renderResults(metrics);
 });
 
-
-// conversion
+// conversion of metrics
 function convertMetrics(input) {
-  const inputedValue = input;
-  const feetValue = roundNumber(inputedValue * metersToFeet);
-  const meterValue = roundNumber(inputedValue * feetsToMeters);
-  const gallonValue = roundNumber(inputedValue * litersToGallons);
-  const litersValue = roundNumber(inputedValue * gallonsToLiters);
-  const poundValue = roundNumber(inputedValue * kilogramsToPounds);
-  const kilogramValue = roundNumber(inputedValue * poundsToKilograms);
+    const inputedValue = Number(input);
 
-  return {
-    feetValue: feetValue,
-    meterValue: meterValue,
-    gallonValue: gallonValue,
-    litersValue: litersValue,
-    poundValue: poundValue,
-    kilogramValue: kilogramValue,
-  };
+  if (typeof inputedValue !== "number" || inputedValue === 0 || isNaN(inputedValue) ) {
+      alert("Enter a valid number");    
+      userInput.value = "";
+  }  else {
+    const feetValue = roundNumber(inputedValue * metersToFeet);
+    const meterValue = roundNumber(inputedValue * feetsToMeters);
+    const gallonValue = roundNumber(inputedValue * litersToGallons);
+    const litersValue = roundNumber(inputedValue * gallonsToLiters);
+    const poundValue = roundNumber(inputedValue * kilogramsToPounds);
+    const kilogramValue = roundNumber(inputedValue * poundsToKilograms);
+      return {
+        feetValue: feetValue,
+        meterValue: meterValue,
+        gallonValue: gallonValue,
+        litersValue: litersValue,
+        poundValue: poundValue,
+        kilogramValue: kilogramValue,
+    };
+  }
+ 
 }
 
-
 function renderResults(metrics) {
-    lengthResults.textContent = `${inputedValue} meters = ${metrics.feetValue} feet | ${inputedValue} feet = ${metrics.meterValue} meters`;
-    volumeResults.textContent = `${inputedValue} liters = ${metrics.gallonValue} gallons | ${inputedValue} gallons = ${metrics.litersValue} liters`;
-    weightResults.textContent = `${inputedValue} kilos = ${metrics.poundValue} pounds | ${inputedValue} pounds = ${metrics.kilogramValue} kilos`;
-  }
-  // round result to 3 decimal places
+  lengthResults.textContent = `${inputedValue} meters = ${metrics.feetValue} feet | ${inputedValue} feet = ${metrics.meterValue} meters`;
+  volumeResults.textContent = `${inputedValue} liters = ${metrics.gallonValue} gallons | ${inputedValue} gallons = ${metrics.litersValue} liters`;
+  weightResults.textContent = `${inputedValue} kilos = ${metrics.poundValue} pounds | ${inputedValue} pounds = ${metrics.kilogramValue} kilos`;
+}
+// round result to 3 decimal places
 function roundNumber(number) {
   return number.toFixed(3);
 }
 
-function checkInput(input) {
-    
-}
+// function checkInput(input) {
+//     const number = Number(input);
+//     if (typeof number !== "number") {
+//         alert("Enter a valid number");
+//     } else {
+//         return number;
+//     }
+// }
